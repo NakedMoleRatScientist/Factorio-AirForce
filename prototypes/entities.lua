9660644bf06b120922b78b7c85e99a70f43454e0
Kiba <hackerkiba@gmail.com>
2016-08-24 21:23:56 -0400

    add an entity

---
+++ prototypes/entities.lua
@@ -0,0 +1,86 @@
+data:extend({
+  {
+    type = "car",
+    name = "car",
+    icon = "__base__/graphics/icons/car.png",
+    flags = {"pushable", "placeable-neutral", "player-creation", "placeable-off-grid"},
+    minable = {mining_time = 1, result = "car"},
+    max_health = 200,
+    corpse = "medium-remnants",
+    dying_explosion = "medium-explosion",
+    energy_per_hit_point = 1,
+    crash_trigger = crash_trigger(),
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 50
+      },
+      {
+        type = "impact",
+        percent = 30,
+        decrease = 30
+      }
+    },
+    collision_box = {{-0.7, -1}, {0.7, 1}},
+    selection_box = {{-0.7, -1}, {0.7, 1}},
+    effectivity = 0.5,
+    braking_power = "200kW",
+    burner =
+    {
+      effectivity = 0.6,
+      fuel_inventory_size = 1,
+      smoke =
+      {
+        {
+          name = "car-smoke",
+          deviation = {0.25, 0.25},
+          frequency = 200,
+          position = {0, 1.5},
+          starting_frame = 0,
+          starting_frame_deviation = 60
+        }
+      }
+    },
+    consumption = "150kW",
+    friction = 2e-3,
+    light =
+    {
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-0.6, -14},
+        size = 2,
+        intensity = 0.6
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {0.6, -14},
+        size = 2,
+        intensity = 0.6
+      }
+    },
+    sound_minimum_speed = 0.2;
+    rotation_speed = 0.015,
+    weight = 700,
+    guns = { "vehicle-machine-gun" },
+    inventory_size = 80
+  }
+})
